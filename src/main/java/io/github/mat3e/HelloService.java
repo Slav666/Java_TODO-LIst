4ef0a1dc8c792dfdbcb5d951ed9b415a2669ef51
Slav666 <slawomirdyk@gmail.com>
2019-12-24 12:29:20 +0000

    Refactore HelloServiceTest code

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -15,9 +15,9 @@ class HelloService {
         this.repository = repository;
     }
 
-     String prepareGreeting(String name) {
-         return prepareGreeting(name, null);
-     }
+//     String prepareGreeting(String name) {
+//         return prepareGreeting(name, null);
+//     }
 
      String prepareGreeting(String name, String lang){
         var langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -1,25 +1,46 @@
 package io.github.mat3e;
 
 import org.junit.Test;
+
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 
 public class HelloServiceTest {
+    private final static String WELCOME = "Hello";
 
-    private HelloService SUT = new HelloService();
+//    private HelloService SUT = new HelloService();
 
     @Test
-    public void test_null_prepare_greeting_returnsFallBackValue(){
-        var result = SUT.prepareGreeting(null);
+    public void test_prepare_greeting_nullName_returnsGreetingWithFallbackName(){
 
-        assertEquals("Hello " + HelloService.FALLBACK_NAME, result);
+        var mockRepository = alwaysReturningHelloRepository();
+
+        var SUT = new HelloService(mockRepository);
+
+        var result = SUT.prepareGreeting(null, "-1");
+
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME, result);
+    };
+
+    @Test
+    public void test_prepare_greeting_name_returnsGreeting_with_name(){
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository);
+
+        String name = "John";
+
+        var result = SUT.prepareGreeting(name, "-1");
+
+        assertEquals(WELCOME + " " + name, result);
     }
-
-    @Test
-    public void test_name_prepare_greeting_returnsGreeting_with_name(){
-        var name = "John";
-        var result = SUT.prepareGreeting(name);
-
-        assertEquals("Hello " + name, result);
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id){
+                return Optional.of(new Lang(null, WELCOME, ""));
+            }
+        };
     }
 }
 
