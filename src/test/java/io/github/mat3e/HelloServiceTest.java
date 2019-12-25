7cea7069c6c7443c3ca62ea9697b4d031700783f
Slav666 <slawomirdyk@gmail.com>
2019-12-25 10:10:58 +0000

    I try to fix error 500, when an user tries to type something different then the language id number. I have written test_prepare_greeting_textLang_returnsGreeting_withFallBackIdLang which does not pass.

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -9,11 +9,12 @@ import static org.junit.Assert.assertEquals;
 
 public class HelloServiceTest {
     private final static String WELCOME = "Hello";
+    private final static String FALLBACK_ID_WELCOME = "Hola";
 
 //    private HelloService SUT = new HelloService();
 
     @Test
-    public void test_prepare_greeting_nullName_returnsGreetingWithFallbackName(){
+    public void test_prepare_greeting_nullName_returnsGreetingWithFallbackName() {
 
         var mockRepository = alwaysReturningHelloRepository();
         var SUT = new HelloService(mockRepository);
@@ -22,40 +23,57 @@ public class HelloServiceTest {
     }
 
     @Test
-    public void test_prepareGreeting_name_returnsGreetingWithName(){
+    public void test_prepareGreeting_name_returnsGreetingWithName() {
         var mockRepository = alwaysReturningHelloRepository();
         var SUT = new HelloService(mockRepository);
-        String name ="John";
+        String name = "John";
         var result = SUT.prepareGreeting(name, "-1");
         assertEquals(WELCOME + " " + name, result);
     }
 
     @Test
-    public void test_prepare_greeting_nullLang_returnsGreeting_withFallBackIdLang(){
+    public void test_prepare_greeting_nullLang_returnsGreeting_withFallBackIdLang() {
 
-        var fallbackIdWelcome = "Hola";
-        var mockRepository = new LangRepository(){
+
+        var mockRepository = fallbackLangIdRepository();
+
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, null);
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME, result);
+    }
+
+    @Test
+    public void test_prepare_greeting_textLang_returnsGreeting_withFallBackIdLang() {
+
+
+        var mockRepository = fallbackLangIdRepository();
+
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "abc");
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME, result);
+    }
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
             @Override
-            Optional<Lang> findById(Long id){
-                if(id.equals(HelloService.FALLBACK_LANG.getId())){
-                    return Optional.of(new Lang(null, fallbackIdWelcome,null));
+            Optional<Lang> findById(Long id) {
+                if (id.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of(new Lang(null, FALLBACK_ID_WELCOME, null));
                 }
                 return Optional.empty();
             }
         };
-        var SUT = new HelloService(mockRepository);
-        var result = SUT.prepareGreeting(null, null);
-        assertEquals(fallbackIdWelcome + " " + HelloService.FALLBACK_NAME, result);
     }
 
-    private LangRepository alwaysReturningHelloRepository() {
-        return new LangRepository(){
-            @Override
-            Optional<Lang> findById(Long id){
-                return Optional.of(new Lang(null, WELCOME, ""));
-            }
-        };
-    }
+        private LangRepository alwaysReturningHelloRepository () {
+            return new LangRepository() {
+                @Override
+                Optional<Lang> findById(Long id) {
+                    return Optional.of(new Lang(null, WELCOME, ""));
+                }
+            };
+        }
 }
+
 
 
