1fc44120e94b1eca0639a26b23b5644deb8519ee
Slav666 <slawomirdyk@gmail.com>
2019-12-25 09:22:13 +0000

    Add test_prepare_greeting_nullLang_returnsGreeting_withFallBackIdLang

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -1,5 +1,6 @@
 package io.github.mat3e;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -15,25 +16,38 @@ public class HelloServiceTest {
     public void test_prepare_greeting_nullName_returnsGreetingWithFallbackName(){
 
         var mockRepository = alwaysReturningHelloRepository();
-
         var SUT = new HelloService(mockRepository);
-
         var result = SUT.prepareGreeting(null, "-1");
-
         assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME, result);
-    };
+    }
 
     @Test
-    public void test_prepare_greeting_name_returnsGreeting_with_name(){
+    public void test_prepareGreeting_name_returnsGreetingWithName(){
         var mockRepository = alwaysReturningHelloRepository();
         var SUT = new HelloService(mockRepository);
-
-        String name = "John";
-
+        String name ="John";
         var result = SUT.prepareGreeting(name, "-1");
-
         assertEquals(WELCOME + " " + name, result);
     }
+
+    @Test
+    public void test_prepare_greeting_nullLang_returnsGreeting_withFallBackIdLang(){
+
+        var fallbackIdWelcome = "Hola";
+        var mockRepository = new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id){
+                if(id.equals(HelloService.FALLBACK_LANG.getId())){
+                    return Optional.of(new Lang(null, fallbackIdWelcome,null));
+                }
+                return Optional.empty();
+            }
+        };
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, null);
+        assertEquals(fallbackIdWelcome + " " + HelloService.FALLBACK_NAME, result);
+    }
+
     private LangRepository alwaysReturningHelloRepository() {
         return new LangRepository(){
             @Override
