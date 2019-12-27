feaded90e739077113d1c2c758122d96761a8c4f
Slav666 <slawomirdyk@gmail.com>
2019-12-27 20:43:41 +0000

    Add two classes: LangService and LangDTO (Data Transfer Object)

---
+++ src/main/java/io/github/mat3e/lang/LangDTO.java
@@ -0,0 +1,27 @@
+package io.github.mat3e.lang;
+
+ class LangDTO {
+     private Integer id;
+     private String code;
+
+     public LangDTO(Lang lang) {
+         this.id = lang.getId();
+         this.code = lang.getCode();
+     }
+
+     public Integer getId() {
+         return id;
+     }
+
+     public String getCode() {
+         return code;
+     }
+
+     public void setId(Integer id) {
+         this.id = id;
+     }
+
+     public void setCode(String code) {
+         this.code = code;
+     }
+ }
